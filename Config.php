a911ef9e299e4f114be386d1ad4644fa4125cea9
shraddha wankhade <shraddha.wankhade@plus91.in>
2012-05-30 16:00:06 +0530

    Initial commit.

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+//Stores values in to variable.
+$S_MysqliHostname="localhost";	//Variable stores Hostname.
+$S_MysqliUser="root";	//Variable stores User.
+$S_MysqliPassword="";	//Variable stores password.
+$S_MysqliDatabase="DrugsDetail";	//Variable stores Database name.
+?>
